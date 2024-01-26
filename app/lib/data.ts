5b07e691bee64f351df3a46c146da9ac89b2716c
carlos stekel <carlosstekel@gmail.com>
2024-01-26 16:05:52 +0100

    Chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,15 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   noStore();
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
